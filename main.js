ccb9ce8b8a1e244f0b1a0c0c019f20bb236bb049
Emmanueladeyemi123 <96444111+Emmanueladeyemi123@users.noreply.github.com>
2022-07-28 18:40:44 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -6,10 +6,31 @@ classifier=ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/4
 }
 
 function modelReady(){
-    classifier.classify(gotResult());
+    classifier.classify(gotResult);
 }
 
-function gotResult(error,result)
+function gotResult(error,results)
 {
-    console.log("Got result");
+    if (error){
+        console.error(error);
+    }
+    if (results[0].label=="cat"){
+        animals.src="cat.jfif";
+    }
+    else if (results[0].label=="dog"){
+        animals.src="dog.jfif";
+    }
+    else if (results[0].label=="cow"){
+        animals.src="cow.jfif";
+    }
+    else{
+        animals.src="cat.jfif";
+    }
+
 }
+
+cat=document.getElementById("cat.jfif");
+dog=document.getElementById("dog.jfif");
+cow=document.getElementById("cow.jfif");
+lion=document.getElementById("lion.jfif");
+
